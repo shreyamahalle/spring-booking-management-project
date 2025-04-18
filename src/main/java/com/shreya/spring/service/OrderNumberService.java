5fb7dda74f7eaa515301397bc34040e72fc0619c
shreya mahalle <shreyamahalle2000@gmail.com>
2025-04-18 17:45:35 +0530

    additional changes added

--- src/main/java/com/shreya/spring/service/OrderNumberService.java
+++ src/main/java/com/shreya/spring/service/OrderNumberService.java
@@ -1,9 +1,9 @@
 package com.shreya.spring.service;
 
 @FunctionalInterface
-public interface OrderNumberService {
-
-    void createOrderNo();
+public interface OrderNumberService
+{
+     void createOrderNo();
 
     //void createOrderNo();
     //void displayOrder();
