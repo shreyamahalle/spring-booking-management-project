9b7a3c5ff91f91fbfe6c9c15182157b5751d7b02
shreya mahalle <shreyamahalle2000@gmail.com>
2025-04-20 10:13:18 +0530

    sql file added

---
+++ src/main/resources/deliveryAgent.sql
@@ -0,0 +1,48 @@
+create table deliveryAgent
+ (id int primary key,name varchar(200) not null,
+  city varchar(200) not null,
+  mobileNo varchar(15) unique not null);
+
+select * from deliveryagent;
+
+insert into deliveryAgent (id, name, city, mobileNo) values (1, 'dcuddehay0', 'Nanshi', 7233455678);
+insert into deliveryAgent (id, name, city, mobileNo) values (2, 'orowantree1', 'Mina de SÃ£o Domingos', 7233485678);
+insert into deliveryAgent (id, name, city, mobileNo) values (3, 'spiddock2', 'GrammatikÃ³', 22333455678);
+insert into deliveryAgent (id, name, city, mobileNo) values (4, 'rcouper3', 'Nyzhni Sirohozy', 7233455678);
+insert into deliveryAgent (id, name, city, mobileNo) values (5, 'ebennet4', 'Yungaisi', 7233455678);
+insert into deliveryAgent (id, name, city, mobileNo) values (6, 'jfriatt5', 'Jintang', 7233455678);
+insert into deliveryAgent (id, name, city, mobileNo) values (7, 'ksemerad6', 'Saint-PÃ©ray', 6233455678);
+insert into deliveryAgent (id, name, city, mobileNo) values (8, 'iosbidston7', 'Lac-Brome', 7233455678);
+insert into deliveryAgent (id, name, city, mobileNo) values (9, 'lhaycraft8', 'Shimodate', 7233455678);
+insert into deliveryAgent (id, name, city, mobileNo) values (10, 'aborwick9', 'Situ', 7233455678);
+insert into deliveryAgent (id, name, city, mobileNo) values (11, 'msmytha', 'ZÃ¡smuky', 7233455678);
+insert into deliveryAgent (id, name, city, mobileNo) values (12, 'rlangloisb', 'Rajadesa', 7233455678);
+insert into deliveryAgent (id, name, city, mobileNo) values (13, 'mmacginleyc', 'Huangjing', 7233455678);
+insert into deliveryAgent (id, name, city, mobileNo) values (14, 'idionisiid', 'Shiqiaotou', 7233455678);
+insert into deliveryAgent (id, name, city, mobileNo) values (15, 'bdurtnele', 'Hultsfred', 7233455678);
+insert into deliveryAgent (id, name, city, mobileNo) values (16, 'nsnelgrovef', 'Mayanhe', 7233455678);
+insert into deliveryAgent (id, name, city, mobileNo) values (17, 'amaideng', 'Kibuku', 7233455678);
+insert into deliveryAgent (id, name, city, mobileNo) values (18, 'aslaghth', 'SÃ£o Mateus', 7233455678);
+insert into deliveryAgent (id, name, city, mobileNo) values (19, 'dcattrelli', 'Alacaygan', 7233455678);
+insert into deliveryAgent (id, name, city, mobileNo) values (20, 'vvanderkruij', 'Sechelt', 7233455678);
+insert into deliveryAgent (id, name, city, mobileNo) values (21, 'acastaniek', 'Fengshan', 7233455678);
+insert into deliveryAgent (id, name, city, mobileNo) values (22, 'langliml', 'Paita', 7233455678);
+insert into deliveryAgent (id, name, city, mobileNo) values (23, 'koluneym', 'PapÃ«r', 7233455678);
+insert into deliveryAgent (id, name, city, mobileNo) values (24, 'cbuggen', 'Armenia', 7233455678);
+insert into deliveryAgent (id, name, city, mobileNo) values (25, 'lpossello', 'Xipi', 7233455678);
+insert into deliveryAgent (id, name, city, mobileNo) values (26, 'jwarlandp', 'Banjar Ponggang', 7233455678);
+insert into deliveryAgent (id, name, city, mobileNo) values (27, 'isparyq', 'Hekou', 7233455678);
+insert into deliveryAgent (id, name, city, mobileNo) values (28, 'dbraneyr', 'Bukama', 7233455678);
+insert into deliveryAgent (id, name, city, mobileNo) values (29, 'rbrisses', 'OstroÅ¾skÃ¡ Lhota', 7233455678);
+insert into deliveryAgent (id, name, city, mobileNo) values (30, 'bmckewt', 'Dawan', 7233455678);
+insert into deliveryAgent (id, name, city, mobileNo) values (31, 'jarpinu', 'Panggungwinong', 8456324780);
+insert into deliveryAgent (id, name, city, mobileNo) values (32, 'snoriegav', 'BanÄ« KhallÄd', 8456324780);
+insert into deliveryAgent (id, name, city, mobileNo) values (33, 'rchristerw', 'Oliveiras', 8456324780);
+insert into deliveryAgent (id, name, city, mobileNo) values (34, 'mgirodx', 'Kuripan', 8456324780);
+insert into deliveryAgent (id, name, city, mobileNo) values (35, 'dlathomy', 'Yishui', 8456324780);
+insert into deliveryAgent (id, name, city, mobileNo) values (36, 'sgilstonz', 'Manaus', 8456324780);
+insert into deliveryAgent (id, name, city, mobileNo) values (37, 'dblaydes10', 'Abuko', 8456324780);
+insert into deliveryAgent (id, name, city, mobileNo) values (38, 'gmacguiness11', 'Jiaoxiyakou', 8456324780);
+insert into deliveryAgent (id, name, city, mobileNo) values (39, 'nhouldey12', 'Tulsa', 8456324780);
+insert into deliveryAgent (id, name, city, mobileNo) values (40, 'zcordingly13', 'Longguang', 8456524780);
+ select * from deliveryagent;
